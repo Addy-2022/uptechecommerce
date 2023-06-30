aae52c3d31693ea72e8149ff31945a19b003f772
adeniran adeoye <adeoyeadeniran624@gmail.com>
2023-06-30 11:17:23 +0100

    new ip

--- uptechcommerce/settings.py
+++ uptechcommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['16.170.253.19','localhost','*']
 
 
 # Application definition
